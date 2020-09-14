7cabcc6beaefb0b46eee902a7985a2c2a700bac2
PIYAWAN NORPALM <63010608@KMITL.AC.TH>
2020-09-14 17:29:12 +0700

    Add project files.

---
+++ Project7/Source.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+	int th,eng,com,math,sci;
+	float pc;
+	printf("Enter Thai score : ");
+	scanf("%d", &th);
+	printf("Enter English score : ");
+	scanf("%d", &eng);
+	printf("Enter Computer score : ");
+	scanf("%d", &com);
+	printf("Enter Math score : ");
+	scanf("%d", &math);
+	printf("Enter Science score : ");
+	scanf("%d", &sci);
+	
+	pc = (th + eng + com + math + sci)/ 5.0;
+	printf("%f", pc);
+
+	if (pc >= 80) {
+		printf("Grade A");
+	}
+	else if (pc >= 70) {
+		printf("Grade B");
+	}
+	else if (pc >= 60) {
+		printf("Grade C");
+	}
+	else if (pc >= 50) {
+		printf("Grade D");
+	}
+	else if (pc <= 50) {
+		printf("Grade F");
+	}
+}
